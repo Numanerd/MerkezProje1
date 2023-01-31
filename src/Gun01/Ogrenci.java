551c48e155d6add170508e815d87defb14b68e22
Numan099 <111380139+Numan099@users.noreply.github.com>
2023-01-31 22:12:37 +0100

    ilk deneme

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -3,6 +3,8 @@ package Gun01;
 public class Ogrenci {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("Merhaab 2");
+        System.out.println();
 
     }
 }
--- src/Gun01/Okul.java
+++
@@ -1,9 +0,0 @@
-package Gun01;
-
-public class Okul {
-    public static void main(String[] args) {
-
-        System.out.println("Merhaba");
-
-    }
-}
